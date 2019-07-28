6951216311b812285a250040d5f38ab074710131
Ravi Jiyani <jiyianiravi@gmail.com>
2019-07-28 17:02:10 +0530

    TDDD Kata updated for requirement '100 should return 2,2,5,5'

--- PrimeFactor/src/main/java/com/af/primefactor/PrimeFactors.java
+++ PrimeFactor/src/main/java/com/af/primefactor/PrimeFactors.java
@@ -1,12 +1,40 @@
 package com.af.primefactor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Based on https://www.programmingwithwolfgang.com/tdd-kata/ : Prime Factor Kata
  */
 public class PrimeFactors {
-	public static List<Integer> generate(int number){
-		return null;
+	
+	public static void main(String[] args) {
+		generate(100);
+	}
+	
+	public static List<Integer> generate(final int number){
+		
+		final int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+		List<Integer> factorsList = new ArrayList<>();
+		
+		int localNumber = number;
+		int currentPrime = 0;
+		for(int i=0; i<number; i++) {
+			currentPrime = primeNumbers[i];
+			while(true) {
+				if(localNumber%currentPrime == 0) {
+					factorsList.add(currentPrime);
+					localNumber/=currentPrime;
+					continue;
+				}
+				break;
+			}
+			
+			if(localNumber == 1) {
+				break;
+			}
+		}
+		
+		return factorsList;
 	} 
 }
--- PrimeFactor/src/test/groovy/com/af/primefactor/PrimeFactors.groovy
+++ PrimeFactor/src/test/groovy/com/af/primefactor/PrimeFactors.groovy
@@ -5,8 +5,19 @@ import spock.lang.Specification
 /**
  * Based on https://www.programmingwithwolfgang.com/tdd-kata/ : Prime Factor Kata
  */
-class PrimeFactors extends Specification {
-	def ''(){
+class PrimeFactorsSpec extends Specification {
+	
+	def 'Specification should return a list of factors for given number'(){
+		given:
+			def result;
+		when:
+			result = PrimeFactors.generate(number);
+		then:
+			expectedResult == result
+		where:
+			number		|		expectedResult
+			100			|	[2,2,5,5]
 		
 	}
+	
 }

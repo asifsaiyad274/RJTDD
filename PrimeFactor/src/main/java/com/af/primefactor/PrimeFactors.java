106535d80be04b6c757eebd58f5db9319c958d1f
Ravi Jiyani <jiyianiravi@gmail.com>
2019-07-28 14:06:04 +0530

    Base commit for PrimeFactor Kata project

---
+++ PrimeFactor/src/main/java/com/af/primefactor/PrimeFactors.java
@@ -0,0 +1,12 @@
+package com.af.primefactor;
+
+import java.util.List;
+
+/**
+ * Based on https://www.programmingwithwolfgang.com/tdd-kata/ : Prime Factor Kata
+ */
+public class PrimeFactors {
+	public static List<Integer> generate(int number){
+		return null;
+	} 
+}
---
+++ PrimeFactor/src/test/groovy/com/af/primefactor/PrimeFactors.groovy
@@ -0,0 +1,12 @@
+package com.af.primefactor
+
+import spock.lang.Specification
+
+/**
+ * Based on https://www.programmingwithwolfgang.com/tdd-kata/ : Prime Factor Kata
+ */
+class PrimeFactors extends Specification {
+	def ''(){
+		
+	}
+}
